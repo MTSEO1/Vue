fc66fa09825c3099c2a698b91ca6551a34db84c8
Seo7829 <104804033+MTSEO1@users.noreply.github.com>
2022-09-16 14:46:18 +0900

    Study Event Key

---
+++ src/views/2_event/EventKeyView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div>
+      <input
+        type="search"
+        name=""
+        id=""
+        v-model="searchTxt"
+        @keyup.enter="doSearch"
+      />
+      <button @click="doSearch">조회</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      searchTxt: ''
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {
+    doSearch() {
+      console.log(this.searchTxt)
+    }
+  }
+}
+</script>
